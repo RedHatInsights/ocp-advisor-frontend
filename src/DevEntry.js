9f6a20ed1bfb511a629f720d5e0c3c4bcae83bd7
Georgii Karataev <gkaratae@redhat.com>
2021-07-02 14:48:17 +0200

    Remove unnecessary files

--- src/DevEntry.js
+++ src/DevEntry.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { init } from './store';
+import { init } from './Store';
 import App from './App';
 import { getBaseName } from '@redhat-cloud-services/frontend-components-utilities/helpers';
 import logger from 'redux-logger';
--- src/Routes.js
+++ src/Routes.js
@@ -6,14 +6,6 @@ import { Bullseye, Spinner } from '@patternfly/react-core';
 const SamplePage = lazy(() =>
   import(/* webpackChunkName: "SamplePage" */ './Routes/SamplePage/SamplePage')
 );
-const OopsPage = lazy(() =>
-  import(/* webpackChunkName: "OopsPage" */ './Routes/OopsPage/OopsPage')
-);
-const NoPermissionsPage = lazy(() =>
-  import(
-    /* webpackChunkName: "NoPermissionsPage" */ './Routes/NoPermissionsPage/NoPermissionsPage'
-  )
-);
 
 /**
  * the Switch component changes routes depending on the path.
@@ -33,8 +25,6 @@ export const Routes = () => (
   >
     <Switch>
       <Route path="/sample" component={SamplePage} />
-      <Route path="/oops" component={OopsPage} />
-      <Route path="/no-permissions" component={NoPermissionsPage} />
       {/* Finally, catch all unmatched routes */}
       <Route>
         <Redirect to="/sample" />
