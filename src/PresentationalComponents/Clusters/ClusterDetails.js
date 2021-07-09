ad5ea00e0feea8b09087fd22c771720372b1cc4d
Georgii Karataev <gkaratae@redhat.com>
2021-07-09 11:22:26 +0200

    Finalize ClusterDetails component

--- src/PresentationalComponents/Clusters/ClusterDetails.js
+++ src/PresentationalComponents/Clusters/ClusterDetails.js
@@ -2,16 +2,19 @@ import React, { useEffect } from 'react';
 import { useIntl } from 'react-intl';
 import PropTypes from 'prop-types';
 import { connect, useDispatch } from 'react-redux';
-import Breadcrumbs from '../../PresentationalComponents/Breadcrumbs/Breadcrumbs';
+import { Grid, GridItem } from '@patternfly/react-core';
 
-import { PageHeader } from '@redhat-cloud-services/frontend-components/PageHeader';
-
+import PageHeader from '@redhat-cloud-services/frontend-components/PageHeader';
+import Main from '@redhat-cloud-services/frontend-components/Main';
 import routerParams from '@redhat-cloud-services/frontend-components-utilities/RouterParams';
 
 import * as AppActions from '../../AppActions';
 import messages from '../../Messages';
+import ClusterDetailHead from '../ClusterDetailHead/ClusterDetailHead';
+import Breadcrumbs from '../../PresentationalComponents/Breadcrumbs/Breadcrumbs';
+import ClusterRecommendations from '../ClusterRecommendations/ClusterRecommendations';
 
-const ClusterDetails = ({ cluster, match }) => {
+const ClusterDetails = ({ cluster, clusterFetchStatus, match }) => {
   const intl = useIntl();
   const dispatch = useDispatch();
 
@@ -25,11 +28,27 @@ const ClusterDetails = ({ cluster, match }) => {
   }, [match.params.clusterId]);
 
   return (
-    <PageHeader className="pf-m-light ins-inventory-detail">
-      {cluster && (
-        <Breadcrumbs current={match.params.clusterId} match={match} />
-      )}
-    </PageHeader>
+    <React.Fragment>
+      <PageHeader className="pf-m-light ins-inventory-detail">
+        {cluster && (
+          <Breadcrumbs current={match.params.clusterId} match={match} />
+        )}
+        <ClusterDetailHead
+          cluster={cluster}
+          clusterFetchStatus={clusterFetchStatus}
+        />
+      </PageHeader>
+      <Main>
+        <Grid hasGutter>
+          <GridItem span={12}>
+            <ClusterRecommendations
+              cluster={cluster}
+              clusterFetchStatus={clusterFetchStatus}
+            />
+          </GridItem>
+        </Grid>
+      </Main>
+    </React.Fragment>
   );
 };
 
