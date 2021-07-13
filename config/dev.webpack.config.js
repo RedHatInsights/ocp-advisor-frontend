8eb7af2257a7ce41f2552bcbf395bd92b0488a28
Martin Marosi <marvusm.mmi@gmail.com>
2021-07-13 11:04:13 +0200

    Fix webpack proxy config.

--- config/dev.webpack.config.js
+++ config/dev.webpack.config.js
@@ -1,13 +1,24 @@
 const { resolve } = require('path');
 const config = require('@redhat-cloud-services/frontend-components-config');
-const { config: webpackConfig, plugins } = config({
-  rootFolder: resolve(__dirname, '../'),
-  debug: true,
+
+const insightsProxy = {
+  https: false,
+  ...(process.env.BETA && { deployment: 'beta/apps' }),
+};
+
+const webpackProxy = {
   deployment: process.env.BETA ? 'beta/apps' : 'apps',
   useProxy: true,
+  useCloud: true,
   appUrl: process.env.BETA
     ? ['/beta/openshift/insights/advisor']
     : ['/openshift/insights/advisor'],
+};
+
+const { config: webpackConfig, plugins } = config({
+  rootFolder: resolve(__dirname, '../'),
+  debug: true,
+  ...(process.env.PROXY ? webpackProxy : insightsProxy),
 });
 
 plugins.push(
