8f7e2ac853eb016e760e2e019eae976ce34b14d0
Georgii Karataev <gkaratae@redhat.com>
2021-08-05 10:50:14 +0200

    Fix fallback component bug

--- src/Components/ReportDetails/ReportDetails.js
+++ src/Components/ReportDetails/ReportDetails.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 
 import AsyncComponent from '@redhat-cloud-services/frontend-components/AsyncComponent';
 
+import Loading from '../Loading/Loading';
+
 // Rule fields processing: https://docs.google.com/document/d/1D0ZLD6-2DZqBX1cUHJY2vL2QAD0dIAzY3O3xFNlEsVg/edit?usp=sharing
 
 const ReportDetails = ({ report }) => {
@@ -11,6 +13,7 @@ const ReportDetails = ({ report }) => {
       <AsyncComponent
         appName="advisor"
         module="./AdvisorReportDetails"
+        fallback={<Loading />}
         // TODO: make API rename `extra_data` to `details` or modify AdvisorReportDetails property
         report={{
           ...report,
