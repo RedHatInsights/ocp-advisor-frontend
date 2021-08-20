84a147169d151923c8222529142bc5b3857c7ffa
Georgii Karataev <gkaratae@redhat.com>
2021-08-20 17:05:42 +0200

    Use useIntl hook instead of injectIntl

--- src/Components/Breadcrumbs/index.js
+++ src/Components/Breadcrumbs/index.js
@@ -3,10 +3,9 @@ import React from 'react';
 import routerParams from '@redhat-cloud-services/frontend-components-utilities/RouterParams';
 
 import { Breadcrumbs } from './Breadcrumbs';
-import { injectIntl } from 'react-intl';
+import { useIntl } from 'react-intl';
 
-export default injectIntl(
-  routerParams(({ match, current, intl }) => (
-    <Breadcrumbs current={current} match={match} intl={intl} />
-  ))
-);
+export default routerParams(({ match, current }) => {
+  const intl = useIntl();
+  <Breadcrumbs current={current} match={match} intl={intl} />;
+});
