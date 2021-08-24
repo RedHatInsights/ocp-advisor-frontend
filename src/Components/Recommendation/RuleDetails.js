6d140ed50e008e8d5cab69b4bb34edf02a524765
Georgii Karataev <gkaratae@redhat.com>
2021-08-24 17:02:55 +0200

    Invoke useIntl for rule details fed module

--- src/Components/Recommendation/RuleDetails.js
+++ src/Components/Recommendation/RuleDetails.js
@@ -1,35 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useIntl } from 'react-intl';
 
 import AsyncComponent from '@redhat-cloud-services/frontend-components/AsyncComponent';
 
 import Loading from '../Loading/Loading';
-import { IntlProvider } from 'react-intl';
 
 const RuleDetails = ({
   rule,
-  intl,
   header,
   isDetailsPage,
   resolutionRisk,
   riskOfChangeDesc,
 }) => {
+  const intl = useIntl();
+
   return (
     <div className="advisor">
-      <IntlProvider locale="en">
-        <AsyncComponent
-          appName="advisor"
-          module="./AdvisorRecommendationDetails"
-          fallback={<Loading />}
-          rule={rule}
-          customItnl
-          intlProps={intl}
-          isDetailsPage={isDetailsPage}
-          header={header}
-          resolutionRisk={resolutionRisk}
-          riskOfChangeDesc={riskOfChangeDesc}
-        />
-      </IntlProvider>
+      <AsyncComponent
+        appName="advisor"
+        module="./AdvisorRecommendationDetails"
+        fallback={<Loading />}
+        rule={rule}
+        customItnl
+        intlProps={intl}
+        isDetailsPage={isDetailsPage}
+        header={header}
+        resolutionRisk={resolutionRisk}
+        riskOfChangeDesc={riskOfChangeDesc}
+      />
     </div>
   );
 };
@@ -37,7 +36,6 @@ const RuleDetails = ({
 RuleDetails.propTypes = {
   header: PropTypes.any,
   isDetailsPage: PropTypes.bool.isRequired,
-  intl: PropTypes.object.isRequired,
   rule: PropTypes.object.isRequired,
   resolutionRisk: PropTypes.number,
   riskOfChangeDesc: PropTypes.string,
