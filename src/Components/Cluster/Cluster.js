6e86d48b48b23bc730da19816c03105c03ab51fa
Aleksandr Voznesenskii <avoznese@redhat.com>
2021-10-11 10:20:27 +0200

    Edited noRecsError and noRecsErrorDesc message and add 2 new props to MessageState component
    I edited the noRecsError and noRecsErrorDesc messages
    
    I created 2 new props(link and the linktext) for the MessageState component and passed the link to the OpenShift docs.

--- src/Components/Cluster/Cluster.js
+++ src/Components/Cluster/Cluster.js
@@ -1,21 +1,21 @@
-import './_Cluster.scss';
+import "./_Cluster.scss";
 
-import React from 'react';
-import PropTypes from 'prop-types';
-import { useIntl } from 'react-intl';
+import React from "react";
+import PropTypes from "prop-types";
+import { useIntl } from "react-intl";
 
-import { Grid, GridItem } from '@patternfly/react-core/dist/js/layouts/Grid';
-import ExclamationCircleIcon from '@patternfly/react-icons/dist/js/icons/exclamation-circle-icon';
-import PageHeader from '@redhat-cloud-services/frontend-components/PageHeader';
-import Main from '@redhat-cloud-services/frontend-components/Main';
-import { global_danger_color_100 as globalDangerColor100 } from '@patternfly/react-tokens/dist/js/global_danger_color_100';
+import { Grid, GridItem } from "@patternfly/react-core/dist/js/layouts/Grid";
+import InfoCircleIcon from "@patternfly/react-icons/dist/js/icons/info-circle-icon";
+import PageHeader from "@redhat-cloud-services/frontend-components/PageHeader";
+import Main from "@redhat-cloud-services/frontend-components/Main";
+import { global_info_color_100 as globalInfoColor100 } from "@patternfly/react-tokens/dist/js/global_info_color_100.js";
 
-import ClusterHeader from '../ClusterHeader';
-import ClusterRules from '../ClusterRules/ClusterRules';
-import Breadcrumbs from '../Breadcrumbs';
-import MessageState from '../MessageState/MessageState';
-import Loading from '../Loading/Loading';
-import messages from '../../Messages';
+import ClusterHeader from "../ClusterHeader";
+import ClusterRules from "../ClusterRules/ClusterRules";
+import Breadcrumbs from "../Breadcrumbs";
+import MessageState from "../MessageState/MessageState";
+import Loading from "../Loading/Loading";
+import messages from "../../Messages";
 
 export const Cluster = ({ cluster, match }) => {
   const intl = useIntl();
@@ -34,8 +34,11 @@ export const Cluster = ({ cluster, match }) => {
           <MessageState
             title={intl.formatMessage(messages.noRecsError)}
             text={intl.formatMessage(messages.noRecsErrorDesc)}
-            icon={ExclamationCircleIcon}
-            iconStyle={{ color: globalDangerColor100.value }}
+            icon={InfoCircleIcon}
+            iconStyle={{ color: globalInfoColor100.value }}
+            variant="large"
+            link="https://docs.openshift.com/container-platform/latest/support/getting-support.html"
+            linktext=" OpenShift documentation"
           />
         </Main>
       )}
--- src/Components/MessageState/MessageState.js
+++ src/Components/MessageState/MessageState.js
@@ -1,14 +1,14 @@
 import {
   EmptyState,
   EmptyStateVariant,
-} from '@patternfly/react-core/dist/js/components/EmptyState/EmptyState';
+} from "@patternfly/react-core/dist/js/components/EmptyState/EmptyState";
 
-import CubesIcon from '@patternfly/react-icons/dist/js/icons/cubes-icon';
-import { EmptyStateBody } from '@patternfly/react-core/dist/js/components/EmptyState/EmptyStateBody';
-import { EmptyStateIcon } from '@patternfly/react-core/dist/js/components/EmptyState/EmptyStateIcon';
-import PropTypes from 'prop-types';
-import React from 'react';
-import { Title } from '@patternfly/react-core/dist/js/components/Title/Title';
+import CubesIcon from "@patternfly/react-icons/dist/js/icons/cubes-icon";
+import { EmptyStateBody } from "@patternfly/react-core/dist/js/components/EmptyState/EmptyStateBody";
+import { EmptyStateIcon } from "@patternfly/react-core/dist/js/components/EmptyState/EmptyStateIcon";
+import PropTypes from "prop-types";
+import React from "react";
+import { Title } from "@patternfly/react-core/dist/js/components/Title/Title";
 
 const MessageState = ({
   className,
@@ -19,15 +19,20 @@ const MessageState = ({
   text,
   title,
   variant,
+  link,
+  linktext,
 }) => (
   <EmptyState className={className} variant={variant}>
-    {icon !== 'none' && (
+    {icon !== "none" && (
       <EmptyStateIcon className={iconClass} style={iconStyle} icon={icon} />
     )}
     <Title headingLevel="h5" size="lg">
       {title}
     </Title>
-    <EmptyStateBody style={{ marginBottom: '16px' }}>{text}</EmptyStateBody>
+    <EmptyStateBody style={{ marginBottom: "16px" }}>
+      {text}
+      <a href={link}>{linktext}</a>
+    </EmptyStateBody>
     {children}
   </EmptyState>
 );
@@ -45,7 +50,7 @@ MessageState.propTypes = {
 
 MessageState.defaultProps = {
   icon: CubesIcon,
-  title: '',
+  title: "",
   variant: EmptyStateVariant.full,
 };
 
