b03992bca117b90c209d3ddbfb0f26dfad37fe94
Georgii Karataev <gkaratae@redhat.com>
2021-10-12 17:47:39 +0200

    Replace workaround with the working v2 endpoint

--- src/Services/SmartProxy.js
+++ src/Services/SmartProxy.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const BASE_URL = '/api/insights-results-aggregator/v1';
+const BASE_URL = '/api/insights-results-aggregator';
 
 export const SmartProxyApi = createApi({
   reducerPath: 'smartProxy',
@@ -10,37 +10,18 @@ export const SmartProxyApi = createApi({
   endpoints: (builder) => ({
     getClusterById: builder.query({
       query: (id, includeDisabled = true) =>
-        `clusters/${id}/report?get_disabled=${includeDisabled}`,
+        `v1/clusters/${id}/report?get_disabled=${includeDisabled}`,
     }),
     // Get rule's content using id (recId = recommendation id) in the rule_plugin_name|error_key format
     getRuleById: builder.query({
-      query: (recId) => `rules/${recId}/content`,
+      query: (recId) => `v1/rules/${recId}/content`,
     }),
     getAffectedClusters: builder.query({
-      query: (recId) => `rule/${recId}/clusters_detail`,
+      query: (recId) => `v1/rule/${recId}/clusters_detail`,
       transformResponse: (response) => response?.data,
     }),
     getRecs: builder.query({
-      // ! FIX BEFORE PATCH PUBLISH
-      query: () => `content`,
-      // ! FIX BEFORE PATCH PUBLISH
-      transformResponse: (response) =>
-        response?.content
-          .filter((v) =>
-            v.plugin.python_module.startsWith('ccx_rules_ocp.external.rules')
-          )
-          .flatMap((current) => {
-            const errorKeys = [];
-            Object.entries(current.error_keys).forEach((entry) => {
-              errorKeys.push({
-                rule_id: current.plugin.python_module + '|' + entry[0],
-                ...current.plugin,
-                ...entry[1],
-                ...entry[1].metadata,
-              });
-            });
-            return errorKeys;
-          }),
+      query: (params) => `v2/rule?impacting=${params.impacting}`,
     }),
   }),
 });
