05b06e7531855e468d21d2c8d41fbca568c42dc1
Aleksandr Voznesenskii <avoznese@redhat.com>
2021-10-24 06:25:21 +0200

    got an react error #321 trying to solve it

--- src/Utilities/ErrorBoundary.js
+++ src/Utilities/ErrorBoundary.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useIntl } from 'react-intl';
 import PropTypes from 'prop-types';
 
-import { Alert, EmptyState } from '@patternfly/react-core';
+import { Alert } from '@patternfly/react-core';
 import { StackItem } from '@patternfly/react-core/dist/js/layouts/Stack';
 import { Title } from '@patternfly/react-core/dist/js/components/Title';
 import messages from '../Messages';
@@ -10,37 +10,32 @@ import messages from '../Messages';
 class ErrorBoundary extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { hasError: false };
+    this.state = { hasError: false, error: null, info: null };
   }
 
-  static getDerivedStateFromError(error) {
-    return { hasError: true };
-  }
-  componentDidCatch(error, errorInfo) {
-    console.log(error, errorInfo);
+  componentDidCatch(error, info) {
+    this.setState({
+      hasError: true,
+      error: error.toString(),
+      info: info,
+    });
   }
 
   render() {
-    const { error, componentStack } = this.state;
     const intl = useIntl();
 
     if (this.state.hasError) {
       return (
-        <EmptyState>
-          <Alert>
-            <div>
-              {error}
-              {componentStack}
-              <Title headingLevel="h4" size="lg">
-                {intl.formatMessage(messages.errorStateTitle)}
-              </Title>
-              <StackItem>
-                {intl.formatMessage(messages.errorStateBody)}
-              </StackItem>
-              <snap>TEST</snap>
-            </div>
-          </Alert>
-        </EmptyState>
+        <Alert>
+          <div>
+            {this.state.error}
+            {this.state.info.componentStack}
+            <Title headingLevel="h4" size="lg">
+              {intl.formatMessage(messages.errorStateTitle)}
+            </Title>
+            <StackItem>{intl.formatMessage(messages.errorStateBody)}</StackItem>
+          </div>
+        </Alert>
       );
     }
 
