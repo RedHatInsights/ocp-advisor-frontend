d29a0d3fc662ef83315322e68ec089a7992401f2
Aleksandr Voznesenskii <avoznese@redhat.com>
2021-10-25 11:17:24 +0200

    added ErrorBoundary and wrapped App component in it

--- src/App.js
+++ src/App.js
@@ -28,7 +28,6 @@ const App = () => {
 
   return (
     <ErrorBoundary>
-      {Error('message')}
       <React.Fragment>
         <NotificationsPortal />
         <Routes />
--- src/Utilities/ErrorBoundary.js
+++ src/Utilities/ErrorBoundary.js
@@ -2,10 +2,7 @@ import React from 'react';
 import { injectIntl } from 'react-intl';
 import PropTypes from 'prop-types';
 
-import { Alert } from '@patternfly/react-core';
-import { StackItem } from '@patternfly/react-core/dist/js/layouts/Stack';
-import { Title } from '@patternfly/react-core/dist/js/components/Title';
-import messages from '../Messages';
+import { ErrorState } from '../Components/MessageState/EmptyStates';
 
 class ErrorBoundary extends React.Component {
   constructor(props) {
@@ -13,34 +10,20 @@ class ErrorBoundary extends React.Component {
     this.state = {
       hasError: false,
       error: null,
-      info: null,
     };
   }
 
-  componentDidCatch(error, info) {
+  componentDidCatch(error) {
     this.setState({
       hasError: true,
       error: error.toString(),
-      info: info,
     });
   }
 
   render() {
-    const { intl } = this.props;
-    const { error, info } = this.state;
+    const { error } = this.state;
     if (error) {
-      return (
-        <Alert>
-          <div>
-            {this.state.error}
-            {info.componentStack}
-            <Title headingLevel="h4" size="lg">
-              {intl.formatMessage(messages.errorStateTitle)}
-            </Title>
-            <StackItem>{intl.formatMessage(messages.errorStateBody)}</StackItem>
-          </div>
-        </Alert>
-      );
+      return <ErrorState />;
     }
 
     return this.props.children;
