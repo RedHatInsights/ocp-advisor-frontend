f9dde7b803ca10b9b396b9ca0bfa0c177997e113
Georgii Karataev <gkaratae@redhat.com>
2021-11-04 12:12:27 +0100

    Always show header for single cluster page
    If display name is available, the header shows it instead of cluster id.

--- src/Components/Cluster/index.js
+++ src/Components/Cluster/index.js
@@ -5,10 +5,12 @@ import { useIntl } from 'react-intl';
 import { useGetClusterByIdQuery } from '../../Services/SmartProxy';
 import messages from '../../Messages';
 import { Cluster } from './Cluster';
+import { useGetClusterDisplayNameByIdQuery } from '../../Services/AccountManagementService';
 
 export default routerParams(({ match }) => {
   const intl = useIntl();
   const cluster = useGetClusterByIdQuery(match.params.clusterId);
+  const displayName = useGetClusterDisplayNameByIdQuery(match.params.clusterId);
 
   useEffect(() => {
     cluster.refetch();
@@ -23,5 +25,5 @@ export default routerParams(({ match }) => {
     }
   }, [match.params.clusterId]);
 
-  return <Cluster cluster={cluster} match={match} />;
+  return <Cluster cluster={cluster} displayName={displayName} match={match} />;
 });
