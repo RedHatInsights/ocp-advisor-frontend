23352c7296daf66356ec492025470cb9c5bde861
Georgii Karataev <gkaratae@redhat.com>
2021-11-30 09:44:27 +0100

    Add enable and disable actions to the rules table

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,14 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 
+import { notificationsMiddleware } from '@redhat-cloud-services/frontend-components-notifications/';
+import { notificationsReducer } from '@redhat-cloud-services/frontend-components-notifications/redux';
+
 import { AmsApi } from '../Services/AccountManagementService';
 import { SmartProxyApi } from '../Services/SmartProxy';
 import filters from '../Services/Filters';
+import { Acks } from '../Services/Acks';
 
 const reducer = {
   [SmartProxyApi.reducerPath]: SmartProxyApi.reducer,
   [AmsApi.reducerPath]: AmsApi.reducer,
   filters,
+  notifications: notificationsReducer,
+  [Acks.reducerPath]: Acks.reducer,
 };
 
 const getStore = (useLogger) =>
@@ -17,7 +23,12 @@ const getStore = (useLogger) =>
     middleware: (getDefaultMiddleware) => {
       const middleware = getDefaultMiddleware().concat(
         SmartProxyApi.middleware,
-        AmsApi.middleware
+        AmsApi.middleware,
+        Acks.middleware,
+        notificationsMiddleware({
+          errorTitleKey: ['message'],
+          errorDescriptionKey: ['response.data.detail'],
+        })
       );
       if (useLogger) {
         middleware.concat(logger);
--- src/Utilities/Api.js
+++ src/Utilities/Api.js
@@ -3,4 +3,10 @@ import axios from 'axios';
 const Post = (url, headers = {}, data = {}) =>
   axios.post(url, data, { headers });
 
-export { Post };
+const Delete = (url, data = {}, headers = {}) => {
+  return axios.delete(url, data, {
+    headers,
+  });
+};
+
+export { Post, Delete };
