4a0230f528f41201f875fadf917601387fd19808
Georgii Karataev <gkaratae@redhat.com>
2022-01-03 17:59:44 +0100

    Render cluster last seen timestamp

--- src/Utilities/Loaders.js
+++ src/Utilities/Loaders.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ContentLoader from 'react-content-loader';
 
 const OneLineLoader = () => (
-  <ContentLoader height={30}>
-    <rect x="0" y="0" rx="4" ry="4" width="300" height="30" />
+  <ContentLoader height={20}>
+    <rect x="0" y="0" rx="4" ry="4" width="300" height="20" />
   </ContentLoader>
 );
 
