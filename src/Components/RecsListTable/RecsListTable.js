31a3226a6750d8ce177fa8e5aa1d63afc4b24773
Fewwy <62722417+Fewwy@users.noreply.github.com>
2022-01-18 10:48:02 +0100

    Compact Labels in Recommendations Table (#87)

--- src/Components/RecsListTable/RecsListTable.js
+++ src/Components/RecsListTable/RecsListTable.js
@@ -119,7 +119,9 @@ const RecsListTable = ({ query }) => {
                 intl.formatMessage(messages.nA)
               ),
             },
-            { title: <CategoryLabel key={key} tags={value.tags} /> },
+            {
+              title: <CategoryLabel key={key} tags={value.tags} />,
+            },
             {
               title: (
                 <div key={key}>
@@ -137,7 +139,10 @@ const RecsListTable = ({ query }) => {
                     )}
                   >
                     {value?.total_risk ? (
-                      <InsightsLabel value={value.total_risk} />
+                      <InsightsLabel
+                        value={value.total_risk}
+                        rest={{ isCompact: true }}
+                      />
                     ) : (
                       intl.formatMessage(messages.nA)
                     )}
