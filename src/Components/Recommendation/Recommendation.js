9cfff11ca4de3c29cd2eca6331b99de5703413a6
Fewwy <62722417+Fewwy@users.noreply.github.com>
2022-01-20 12:23:42 +0100

    Fix incorrect POST /ack request (#92)

--- src/Components/Recommendation/Recommendation.js
+++ src/Components/Recommendation/Recommendation.js
@@ -117,7 +117,7 @@ const Recommendation = ({ rule, ack, clusters, match }) => {
 
   const enableRule = async (rule) => {
     try {
-      await Delete(`${BASE_URL}/ack/${rule.rule_id}/`);
+      await Delete(`${BASE_URL}/v2/ack/${rule.data.content.rule_id}/`);
       notify({
         variant: 'success',
         timeout: true,
