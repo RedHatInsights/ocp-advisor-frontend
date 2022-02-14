bc0644c6815279e87492cd70ba63c2e593259122
Iker Reyes <ireyes@redhat.com>
2022-02-14 17:20:53 +0100

    Add components and defaults

--- cypress/utils/components.js
+++ cypress/utils/components.js
@@ -5,5 +5,7 @@ const ROW = '[data-ouia-component-type="PF4/TableRow"]';
 const PAGINATION = 'div[data-ouia-component-type="PF4/Pagination"]';
 const PAGINATION_MENU =
   'div[data-ouia-component-type="PF4/PaginationOptionsMenu"]';
+const DROPDOWN_TOGGLE = '[data-ouia-component-type="PF4/DropdownToggle"]'
+const DROPDOWN_ITEM = '[data-ouia-component-type="PF4/DropdownItem"]'
 
-export { TOOLBAR, CHIP_GROUP, CHIP, ROW, PAGINATION, PAGINATION_MENU };
+export { TOOLBAR, CHIP_GROUP, CHIP, ROW, PAGINATION, PAGINATION_MENU, DROPDOWN_TOGGLE, DROPDOWN_ITEM };
