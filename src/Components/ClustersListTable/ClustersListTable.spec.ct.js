49ba733886f640ddecdb7c828a32e301d7e84f64
Iker Reyes <ireyes@redhat.com>
2022-02-14 17:22:01 +0100

    Improve ClusterListTable tests

--- src/Components/ClustersListTable/ClustersListTable.spec.ct.js
+++ src/Components/ClustersListTable/ClustersListTable.spec.ct.js
@@ -2,19 +2,118 @@ import React from 'react';
 import { mount } from '@cypress/react';
 import { MemoryRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import _ from 'lodash';
 
 import { Intl } from '../../Utilities/intlHelper';
 import getStore from '../../Store';
 import { ClustersListTable } from './ClustersListTable';
 import props from '../../../cypress/fixtures/ClustersListTable/data.json';
 import '@patternfly/patternfly/patternfly.scss';
+import {
+  TOOLBAR,
+  ROW,
+  PAGINATION,
+  PAGINATION_MENU,
+  CHIP_GROUP,
+  DROPDOWN_TOGGLE,
+  DROPDOWN_ITEM,
+} from '../../../cypress/utils/components';
+import {
+  DEFAULT_ROW_COUNT,
+  PAGINATION_VALUES,
+} from '../../../cypress/utils/defaults';
+
+const data = props['data'];
+// add property name to clusters
+let namedClusters = _.cloneDeep(data);
+namedClusters.forEach(
+  (it) =>
+    (it['name'] = it['cluster_name'] ? it['cluster_name'] : it['cluster_id'])
+);
+
+const TABLE = 'div[id=clusters-list-table]';
+const TABLE_HEADERS = [
+  'Name',
+  'Recommendations',
+  'Critical',
+  'Important',
+  'Moderate',
+  'Low',
+  'Last seen',
+];
+
+function checkRowCounts(n) {
+  return cy
+    .get('table tbody[role=rowgroup]')
+    .find(ROW)
+    .should('have.length', n);
+}
+
+function checkPaginationTotal(n) {
+  return cy
+    .get('.pf-c-options-menu__toggle-text')
+    .find('b')
+    .eq(1)
+    .should('have.text', n);
+}
+
+// FIXME improve syntax
+// FIXME move to utils module
+function itemsPerPage() {
+  let items = data.length;
+  const array = [];
+  while (items > 0) {
+    const remain = items - DEFAULT_ROW_COUNT;
+    let v = remain > 0 ? DEFAULT_ROW_COUNT : items;
+    array.push(v);
+    items = remain;
+  }
+  return array;
+}
+
+describe('data', () => {
+  it('has values', () => {
+    cy.wrap(data).its('length').should('be.gte', 1);
+  });
+  it('has more entried than default pagination', () => {
+    cy.wrap(data).its('length').should('be.gt', DEFAULT_ROW_COUNT);
+  });
+  it('at least one cluster has cluster name', () => {
+    cy.wrap(_.filter(data, (it) => it.cluster_name))
+      .its('length')
+      .should('be.gte', 1);
+  });
+  it('first cluster has name', () => {
+    cy.wrap(data[0]['cluster_name']).should('not.be.empty');
+  });
+  it('first page items contains at least one cluster without name', () => {
+    const itemsInFirstPage = Math.min(DEFAULT_ROW_COUNT, data.length);
+    cy.wrap(_.filter(data.slice(0, itemsInFirstPage), (it) => it.cluster_name))
+      .its('length')
+      .should('be.lt', itemsInFirstPage);
+  });
+  it('at least one entry has last seen', () => {
+    cy.wrap(_.filter(data, (it) => it.last_checked_at))
+      .its('length')
+      .should('be.gte', 1);
+  });
+  it('at least one entry does not have last seen', () => {
+    cy.wrap(_.filter(data, (it) => it.last_checked_at === ''))
+      .its('length')
+      .should('be.gte', 1);
+  });
+  it('at least one entry does not have all values for total risk categories', () => {
+    cy.wrap(
+      _.filter(data, (it) => Object.keys(it['hits_by_total_risk']).length)
+    )
+      .its('length')
+      .should('be.gte', 1);
+  });
+});
 
 describe('clusters list table', () => {
-  const CLUSTERS_LIST_TABLE = 'div[id=clusters-list-table]';
   const TBODY = 'tbody[role=rowgroup]';
-  const CHIP_GROUP = '.pf-c-chip-group__main';
   const TOOLBAR_FILTER = '.ins-c-primary-toolbar__filter';
-  const PAGINATION = '.pf-c-pagination';
 
   Cypress.Commands.add('getTotalClusters', () =>
     cy.get('.pf-c-options-menu__toggle-text').find('b').eq(1)
@@ -43,19 +142,77 @@ describe('clusters list table', () => {
   });
 
   it('renders table', () => {
-    cy.get(CLUSTERS_LIST_TABLE).should('have.length', 1);
+    cy.get(TABLE).within(() => {
+      cy.get(TOOLBAR).should('have.length', 1);
+      cy.get('table').should('have.length', 1);
+    });
   });
 
-  it('shows 29 clusters as a total number', () => {
-    cy.getTotalClusters().should('have.text', 29);
+  it('shows first clusters only', () => {
+    checkRowCounts(DEFAULT_ROW_COUNT);
   });
 
-  it('pagination default is set to 20', () => {
-    cy.get(TBODY).children().should('have.length', 20);
+  it('shows correct total number of clusters', () => {
+    checkPaginationTotal(data.length);
+  });
+
+  it('shows table headers', () => {
+    cy.get('table th')
+      .then(($els) => {
+        return _.map(Cypress.$.makeArray($els), 'innerText');
+      })
+      .should('deep.equal', TABLE_HEADERS);
+  });
+
+  it(`pagination default is set to ${DEFAULT_ROW_COUNT}`, () => {
     cy.get('.pf-c-options-menu__toggle-text')
       .find('b')
       .eq(0)
       .should('have.text', '1 - 20');
+  });
+
+  it('can change page limit', () => {
+    // FIXME: best way to make the loop
+    cy.wrap(PAGINATION_VALUES).each((el) => {
+      cy.get(TOOLBAR).find(PAGINATION_MENU).find(DROPDOWN_TOGGLE).click();
+      cy.get(TOOLBAR)
+        .find(PAGINATION_MENU)
+        .find('ul[class=pf-c-options-menu__menu]')
+        .find(DROPDOWN_ITEM)
+        .contains(`${el}`)
+        .click({ force: true }); // caused by the css issue
+      checkRowCounts(Math.min(el, data.length));
+    });
+  });
+
+  it('pagination defaults are expected ones', () => {
+    cy.get(TOOLBAR).find(PAGINATION_MENU).find(DROPDOWN_TOGGLE).click();
+    cy.get(TOOLBAR)
+      .find(PAGINATION_MENU)
+      .find('ul[class=pf-c-options-menu__menu]')
+      .find('li')
+      .each(($el, index) => {
+        cy.wrap($el).should(
+          'have.text',
+          `${PAGINATION_VALUES[index]} per page`
+        );
+      });
+  });
+
+  it('can iterate over pages', () => {
+    cy.wrap(itemsPerPage()).each((el, index, list) => {
+      checkRowCounts(el);
+      cy.get(TOOLBAR)
+        .find(PAGINATION)
+        .find('button[data-action="next"]')
+        .then(($button) => {
+          if (index === list.length - 1) {
+            cy.wrap($button).should('be.disabled');
+          } else {
+            cy.wrap($button).click();
+          }
+        });
+    });
   });
 
   it('applies one total risk filter as a default', () => {
@@ -67,6 +224,13 @@ describe('clusters list table', () => {
       .should('have.length', 1)
       .should('have.text', 'All clusters');
   });
+
+  it('reset filters button is displayed', () => {
+    cy.get('button').contains('Reset filters').should('exist');
+  });
+
+  // TODO improve filtering tests
+  // TODO check that empty table is displayed with appropriate filters
 
   it('can filter out only hitting clusters', () => {
     // initially there are 29 clusters
@@ -138,21 +302,23 @@ describe('clusters list table', () => {
       .should('have.text', 0);
   });
 
-  it('some rows have cluster names instead uuids', () => {
-    /* the cluster with uuid "fc603601-0ff8-45e4-b0f3-c7f76d2ef36b"
-       has a display name "gsbq8pthf xah3olxhz" */
-    cy.get(TBODY)
-      .children()
-      .eq(4)
-      .find('td[data-label=Name]')
-      .should('have.text', '5uq3oy111 ufq7fnxcd');
+  it('rows show cluster names instead uuids when available', () => {
+    const names = _.map(namedClusters, 'name');
+    cy.get(`td[data-label="Name"]`)
+      .then(($els) => {
+        return _.map(Cypress.$.makeArray($els), 'innerText');
+      })
+      .should(
+        'deep.equal',
+        names.slice(0, Math.min(DEFAULT_ROW_COUNT, names.length))
+      );
   });
 
   it('names of rows are links', () => {
     cy.getFirstRow()
       .find('td[data-label=Name]')
-      .find('a[href="/clusters/e488c993-821c-4915-bd08-5a51ed7aa3a2"]')
-      .should('have.text', 'gvgubed6h jzcmr99ojh');
+      .find(`a[href="/clusters/${data[0]['cluster_id']}"]`)
+      .should('have.text', data[0]['cluster_name']);
   });
 
   it('sorts N/A in last seen correctly', () => {
@@ -171,6 +337,57 @@ describe('clusters list table', () => {
       .find('button')
       .click({ force: true });
     cy.getLastRow().find('span').should('have.text', 'N/A');
+  });
+
+  // TODO fix test: double sorting? keep ordering in single sorting?
+  Object.entries({
+    name: 'Name',
+    total_hit_count: 'Recommendations',
+    'hits_by_total_risk.4': 'Critical',
+    'hits_by_total_risk.3': 'Important',
+    'hits_by_total_risk.2': 'Moderate',
+    'hits_by_total_risk.1': 'Low',
+    last_checked_at: 'Last seen',
+  }).forEach(([category, label]) => {
+    ['ascending', 'descending'].forEach((order) => {
+      it(`sort ${order} by ${label}`, () => {
+        const col = `td[data-label="${label}"]`;
+        const header = `th[data-label="${label}"]`;
+
+        cy.get(col).should(
+          'have.length',
+          Math.min(DEFAULT_ROW_COUNT, namedClusters.length)
+        );
+        cy.get(header).find('button').click();
+        // FIXME right way to do the second click?
+        if (order === 'descending') {
+          // click a second time to reverse sorting
+          cy.get(header).find('button').click();
+        }
+
+        // add property name to clusters
+        let sortedNames = _.map(
+          // preserver original ordering
+          _.orderBy(
+            _.cloneDeep(namedClusters),
+            [category],
+            [order === 'descending' ? 'desc' : 'asc']
+          ),
+          'name'
+        );
+        cy.get(`td[data-label="Name"]`)
+          .then(($els) => {
+            return _.map(Cypress.$.makeArray($els), 'innerText');
+          })
+          .should(
+            'deep.equal',
+            sortedNames.slice(
+              0,
+              Math.min(DEFAULT_ROW_COUNT, sortedNames.length)
+            )
+          );
+      });
+    });
   });
 });
 
