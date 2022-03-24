5961ef7c2cade9593ec446ea09b0af0eeba595a5
Georgii Karataev <gkaratae@redhat.com>
2022-03-24 09:19:00 +0100

    fix(tables): revert the time-based refresh feature
    This reverts the feature of data time-based refresh (was set to 20
    seconds) in the clusters list and recommendations list tables.

--- src/Components/RecsListTable/RecsListTable.js
+++ src/Components/RecsListTable/RecsListTable.js
@@ -112,8 +112,6 @@ const RecsListTable = ({ query }) => {
     setFilterBuilding(false);
   }, []);
 
-  useEffect(() => setInterval(() => refetch(), 20000), []);
-
   // constructs array of rows (from the initial data) checking currently applied filters
   const buildFilteredRows = (allRows, filters) => {
     return allRows
