ba7a021603c5b9a53f1c8d0972307c84ad054983
Iker Reyes <ireyes@redhat.com>
2022-04-29 12:01:50 +0200

    Removed unused variables

--- cypress/utils/globals.js
+++ cypress/utils/globals.js
@@ -8,17 +8,6 @@ const CATEGORIES = {
   Performance: ['performance'],
 };
 
-// TODO remove if unused
-// const CATEGORY_MAP = {
-//   'Service Availability': 1,
-//   Performance: 2,
-//   Security: 4,
-//   'Fault Tolerance': 3,
-// };
-
-// const IMPACT = { Low: 1, Medium: 2, High: 3, Critical: 4 };
-// const LIKELIHOOD = { Low: 1, Medium: 2, High: 3, Critical: 4 };
-
 const SORTING_ORDERS = ['ascending', 'descending'];
 
 export { TOTAL_RISK, CATEGORIES, SORTING_ORDERS };
