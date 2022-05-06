9d082bfec7d94b37935b8317f0648d40549e89bf
Iker Reyes <ireyes@redhat.com>
2022-05-06 13:32:58 +0200

    Add TODO note

--- src/Components/ClustersListTable/ClustersListTable.spec.ct.js
+++ src/Components/ClustersListTable/ClustersListTable.spec.ct.js
@@ -544,3 +544,5 @@ describe('cluster list Empty state rendering', () => {
       .should('have.text', 'Assisted Installer clusters');
   });
 });
+
+// TODO tests for URL parameters and chips as in RecsListTable
