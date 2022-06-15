f6fd7d1443af1e29e8fd789223b91976988b719d
Iker Reyes <ireyes@redhat.com>
2022-06-15 10:45:54 +0200

    Check label using right locator

--- src/Components/RecsListTable/RecsListTable.spec.ct.js
+++ src/Components/RecsListTable/RecsListTable.spec.ct.js
@@ -700,20 +700,17 @@ describe('successful non-empty recommendations list table', () => {
   });
 
   describe('enabling/disabling', () => {
-    it.only('disabled rule has a label', () => {
+    it('disabled rule has a label', () => {
       removeAllChips();
       filterApply({ status: 'Disabled' });
       // according to data specs there should be at least 1 disabled row
-      cy.get(`td[data-label="Name"]`)
-        .then(($els) => {
-          return _.map(Cypress.$.makeArray($els), 'innerText');
-        })
-        .each((it) => {
-          assert.isOk(
-            it.endsWith(' \nDisabled'),
-            'Disabled rule includes disabled label'
-          );
+      cy.get(`td[data-label="Name"]`).then(($els) => {
+        cy.wrap($els).each(($el) => {
+          cy.wrap($el)
+            .find('span[class=pf-c-label__content]')
+            .should('have.text', 'Disabled');
         });
+      });
     });
 
     it('each row has a kebab', () => {
