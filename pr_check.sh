3b54e3031ad8823679859ac34a18c2802dfb8367
Iker Reyes <ireyes@redhat.com>
2023-01-26 13:58:59 +0100

    export COMPONENT_NAME

--- pr_check.sh
+++ pr_check.sh
@@ -27,7 +27,7 @@ export IQE_CJI_TIMEOUT="30m"
 
 REF_ENV="insights-production"
 #COMPONENTS_W_RESOURCES=""
-COMPONENT_NAME="ocp-advisor
+export COMPONENT_NAME="ocp-advisor
 #COMPONENTS="ccx-data-pipeline ccx-insights-results insights-content-service insights-results-smart-proxy"  # space-separated list of components to laod"
 
 
