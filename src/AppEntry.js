23d7f28c618ed3c74680900872d8cff580c53dac
Martin Maroši <marvusm.mmi@gmail.com>
2023-07-25 10:46:45 +0200

    fix: Remove obsolete APP_NAVIGATION listener (#596)
    Fixes https://issues.redhat.com/browse/RHCLOUD-27284.

--- src/AppEntry.js
+++ src/AppEntry.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { getBaseName } from '@redhat-cloud-services/frontend-components-utilities/helpers/helpers';
 import App from './App';
 import { Intl } from './Utilities/intlHelper';
 
 const AppEntry = ({ useLogger }) => {
   return (
     <Intl>
-      <App
-        useLogger={useLogger}
-        basename={getBaseName(window.location.pathname)}
-      />
+      <App useLogger={useLogger} />
     </Intl>
   );
 };
