6211b40225be9566273ac4b19e512aca2ee636af
Fewwy <avoznese@redhat.com>
2023-10-26 09:57:36 +0200

    feat(OCPADVISOR-124): workloads feature flag

--- src/Utilities/useFeatureFlag.js
+++ src/Utilities/useFeatureFlag.js
@@ -12,9 +12,18 @@ export default useFeatureFlag;
 export const UPDATE_RISKS_ENABLE_FLAG =
   'ocp-advisor.upgrade-risks.enable-in-stable';
 
+export const WORKLOADS_ENABLE_FLAG = 'ocp-advisor-ui-workloads';
+
 export const useUpdateRisksFeatureFlag = () => {
   const updateRisksEnabled = useFeatureFlag(UPDATE_RISKS_ENABLE_FLAG);
   const chrome = useChrome();
 
   return chrome.isBeta() || updateRisksEnabled;
+};
+
+export const useWorkloadsFeatureFlag = () => {
+  const workloadsEnabled = useFeatureFlag(WORKLOADS_ENABLE_FLAG);
+  const chrome = useChrome();
+
+  return chrome.isBeta() || workloadsEnabled;
 };
