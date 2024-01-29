62d480d132d221d5b969aeb748908a2823dd9c27
Alexandr Čelakovský <20592948+AsToNlele@users.noreply.github.com>
2024-01-29 15:36:12 +0100

    feat(RHINENG-7723): workload replace API endpoints with mock data (#660)

--- src/Components/WorkloadsListTable/index.js
+++ src/Components/WorkloadsListTable/index.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import { useGetWorkloadsQuery } from '../../Services/SmartProxy';
+// import { useGetWorkloadsQuery } from '../../Services/SmartProxy';
 import { WorkloadsListTable } from './WorkloadsListTable';
-// import mockdata from '../../../cypress/fixtures/api/insights-results-aggregator/v2/workloads.json';
+import mockdata from '../../../cypress/fixtures/api/insights-results-aggregator/v2/workloads.json';
 
 const WorkloadsListTableWrapper = () => {
-  const query = useGetWorkloadsQuery();
-
+  // const query = useGetWorkloadsQuery();
   // For more diverse use mockdata
   // Comment out the query above
   // Uncomment the query below
   // Uncomment the mockdata import above
-  // const query = {
-  //   isError: false,
-  //   isFetching: false,
-  //   isUninitialized: false,
-  //   isSuccess: true,
-  //   data: { workloads: mockdata },
-  //   refetch: () => null,
-  // };
+  const query = {
+    isError: false,
+    isFetching: false,
+    isUninitialized: false,
+    isSuccess: true,
+    data: { workloads: mockdata },
+    refetch: () => null,
+  };
 
   return <WorkloadsListTable query={query} />;
 };
