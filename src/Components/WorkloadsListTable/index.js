06c50100c425b19c48aaadba9d8610f00950aad6
Fewwy <avoznese@redhat.com>
2024-03-04 12:37:59 +0100

    feat(workloads): removed mocked data

--- src/Components/WorkloadsListTable/index.js
+++ src/Components/WorkloadsListTable/index.js
@@ -1,23 +1,9 @@
 import React from 'react';
-// import { useGetWorkloadsQuery } from '../../Services/SmartProxy';
+import { useGetWorkloadsQuery } from '../../Services/SmartProxy';
 import { WorkloadsListTable } from './WorkloadsListTable';
-import mockdata from '../../../cypress/fixtures/api/insights-results-aggregator/v2/workloads.json';
 
 const WorkloadsListTableWrapper = () => {
-  // const query = useGetWorkloadsQuery();
-  // For more diverse use mockdata
-  // Comment out the query above
-  // Uncomment the query below
-  // Uncomment the mockdata import above
-  const query = {
-    isError: false,
-    isFetching: false,
-    isUninitialized: false,
-    isSuccess: true,
-    data: { workloads: mockdata },
-    refetch: () => null,
-  };
-
+  const query = useGetWorkloadsQuery();
   return <WorkloadsListTable query={query} />;
 };
 
