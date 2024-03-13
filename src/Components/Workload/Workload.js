a7d9ee6c52b989c96d13e20867e172a5ddb02478
Fewwy <avoznese@redhat.com>
2024-03-13 16:18:14 +0100

    fix(bug): remove redundand api call

--- src/Components/Workload/Workload.js
+++ src/Components/Workload/Workload.js
@@ -20,7 +20,11 @@ export const Workload = ({ workload, namespaceId, clusterId }) => {
               }
               workloads="true"
             />
-            <WorkloadsHeader />
+            <WorkloadsHeader
+              workload={workload}
+              namespaceId={namespaceId}
+              clusterId={namespaceId}
+            />
           </FlexItem>
         </Flex>
       </PageHeader>
--- src/Components/WorkloadHeader/index.js
+++ src/Components/WorkloadHeader/index.js
@@ -1,22 +1,37 @@
 import React from 'react';
 import { WorkloadHeader } from './WorkloadHeader';
-import { useParams } from 'react-router-dom';
-import { useGetWorkloadByIdQuery } from '../../Services/SmartProxy';
+import PropTypes from 'prop-types';
 
-const WorkloadsHeaderWrapper = () => {
-  const { namespaceId, clusterId } = useParams();
-  const workloadData = useGetWorkloadByIdQuery({
-    namespaceId,
-    clusterId,
-  });
-
+const WorkloadsHeaderWrapper = ({ workload, namespaceId, clusterId }) => {
   return (
     <WorkloadHeader
-      workloadData={workloadData}
+      workloadData={workload}
       namespaceId={namespaceId}
       clusterId={clusterId}
     />
   );
+};
+WorkloadsHeaderWrapper.propTypes = {
+  clusterId: PropTypes.string.isRequired,
+  namespaceId: PropTypes.string.isRequired,
+  workload: PropTypes.shape({
+    isUninitialized: PropTypes.bool.isRequired,
+    isFetching: PropTypes.bool.isRequired,
+    data: PropTypes.shape({
+      metadata: PropTypes.shape({
+        last_checked_at: PropTypes.string,
+      }),
+      namespace: PropTypes.shape({
+        uuid: PropTypes.string,
+        name: PropTypes.string,
+      }),
+      cluster: PropTypes.shape({
+        uuid: PropTypes.string,
+        display_name: PropTypes.string,
+      }),
+      status: PropTypes.string,
+    }),
+  }),
 };
 
 export default WorkloadsHeaderWrapper;
