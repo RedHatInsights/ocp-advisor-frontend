4b1257e376f2c44570c0f08a93d071cbec6552be
Aleksandr Voznesenskii <62722417+Fewwy@users.noreply.github.com>
2024-03-26 16:59:41 +0100

    feat(RHINENG-7049): fec config setup (#720)
    * feat(RHINENG-7049): fec config setup
    
    * removed redundant start command and updated readme

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,3 @@
-const path = require('path');
-const glob = require('glob');
-
 module.exports = {
   presets: [
     // Polyfills
@@ -16,48 +13,6 @@ module.exports = {
     // Devs tend to write `import { someIcon } from '@patternfly/react-icons';`
     // This transforms the import to be specific which prevents having to parse 2k+ icons
     // Also prevents potential bundle size blowups with CJS
-    [
-      'transform-imports',
-      {
-        '@patternfly/react-core': {
-          transform: (importName) => {
-            let res;
-            const files = glob.sync(
-              path.resolve(
-                __dirname,
-                `./node_modules/@patternfly/react-core/dist/js/**/${importName}.js`
-              )
-            );
-            if (files.length > 0) {
-              res = files[0];
-            } else {
-              throw `File with importName ${importName} does not exist`;
-            }
-
-            res = res.replace(path.resolve(__dirname, './node_modules/'), '');
-            res = res.replace(/^\//, '');
-            return res;
-          },
-          preventFullImport: false,
-          skipDefaultConversion: true,
-        },
-      },
-      'react-core',
-    ],
-    [
-      'transform-imports',
-      {
-        '@patternfly/react-icons': {
-          transform: (importName) =>
-            `@patternfly/react-icons/dist/js/icons/${importName
-              .split(/(?=[A-Z])/)
-              .join('-')
-              .toLowerCase()}`,
-          preventFullImport: true,
-        },
-      },
-      'react-icons',
-    ],
     [
       'formatjs',
       {
---
+++ fec.config.js
@@ -0,0 +1,36 @@
+const { resolve } = require('path');
+
+module.exports = {
+  appUrl: '/openshift/insights/advisor',
+  debug: true,
+  useProxy: process.env.PROXY === 'true',
+  proxyVerbose: true,
+  /**
+   * Change to false after your app is registered in configuration files
+   */
+  sassPrefix: 'ocp-advisor, ocpAdvisor',
+  interceptChromeConfig: false,
+  /**
+   * Add additional webpack plugins
+   */
+  plugins: [],
+  ...(process.env.HOT
+    ? { hotReload: process.env.HOT === 'true' }
+    : { hotReload: true }),
+  ...(process.env.port ? { port: parseInt(process.env.port) } : {}),
+  moduleFederation: {
+    exclude: ['react-router-dom'],
+    shared: [
+      {
+        'react-router-dom': {
+          singleton: true,
+          import: false,
+          version: '^6.3.0',
+        },
+      },
+    ],
+  },
+  exposes: {
+    './RootApp': resolve(__dirname, '../src/AppEntry'),
+  },
+};
