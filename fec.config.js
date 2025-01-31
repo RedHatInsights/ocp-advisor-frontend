cc6fdf433ef3144878ac5678a7ba9f2bbc816990
Adonis Puente <60629070+adonispuente@users.noreply.github.com>
2025-01-31 16:16:46 -0600

    feat(RHINENG-15390): Onboard OCP Advisor to Sentry Instance (#879)

--- fec.config.js
+++ fec.config.js
@@ -1,4 +1,5 @@
 const { resolve } = require('path');
+const { sentryWebpackPlugin } = require('@sentry/webpack-plugin');
 
 module.exports = {
   appUrl: '/openshift/insights/advisor',
@@ -12,7 +13,27 @@ module.exports = {
   /**
    * Add additional webpack plugins
    */
-  plugins: [],
+  devtool: 'hidden-source-map',
+  plugins: [
+    // Put the Sentry Webpack plugin after all other plugins
+    ...(process.env.ENABLE_SENTRY
+      ? [
+          sentryWebpackPlugin({
+            ...(process.env.SENTRY_AUTH_TOKEN && {
+              authToken: process.env.SENTRY_AUTH_TOKEN,
+            }),
+            org: 'red-hat-it',
+            project: 'ocp-advisor',
+            moduleMetadata: ({ release }) => ({
+              dsn: `https://27daee0fa0238ac7f7d5389b8ac8f825@o490301.ingest.us.sentry.io/4508683272454144`,
+              org: 'red-hat-it',
+              project: 'ocp-advisor',
+              release,
+            }),
+          }),
+        ]
+      : []),
+  ],
   ...(process.env.HOT
     ? { hotReload: process.env.HOT === 'true' }
     : { hotReload: true }),
