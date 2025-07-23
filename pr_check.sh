e74585831884e8e3412dd027317d6913ac3b3cf6
Michael Johnson <micjohns@micjohns-thinkpadp16vgen1.rmtusnc.csb>
2025-07-23 10:19:41 -0400

    chore(RHINENG-16644): Update to Node version 22

--- pr_check.sh
+++ pr_check.sh
@@ -9,8 +9,8 @@ export COMPONENT="ocp-advisor"
 export IMAGE="quay.io/cloudservices/ocp-advisor-frontend"
 export WORKSPACE=${WORKSPACE:-$APP_ROOT} # if running in jenkins, use the build's workspace
 export APP_ROOT=$(pwd)
-# 16 is the default Node version. Change this to override it.
-export NODE_BUILD_VERSION=18
+
+export NODE_BUILD_VERSION=22
 COMMON_BUILDER=https://raw.githubusercontent.com/RedHatInsights/insights-frontend-builder-common/master
 
 # --------------------------------------------
